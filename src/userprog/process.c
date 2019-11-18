8f3c31174780291436a119ad061bf3e72639c705
Taeyoon <cloudloverty@kaist.ac.kr>
2019-11-18 17:29:18 +0900

    resolve conflict 2

--- src/userprog/process.c
+++ src/userprog/process.c
@@ -674,7 +674,6 @@ setup_stack (void **esp)
     {
       upage = ((uint8_t *) PHYS_BASE) - PGSIZE;
       success = install_page (upage, kpage, true);
-<<<<<<< HEAD
       if (success) 
 	  {
 		  *esp = PHYS_BASE;
@@ -700,26 +699,7 @@ setup_stack (void **esp)
 		  palloc_free_page(kpage);
 		  return false;
 	  }
-=======
-      if (success) {
-        ////////// Added in P3 start /////////
-        *esp = PHYS_BASE;
-        //initialize vm_entry for 4KB stack 
-        vme = malloc(sizeof (struct vm_entry));
 
-        //set members of the vm_entry 
-        //TODO: va, write_permission, is_loaded_to_memory unclear
-        vme->file_type = VM_SWAP;
-        vme->va = upage;                
-        vme->write_permission = true;    
-        vme->is_loaded_to_memory = true;
-
-        //insert created vm_entry into vm hash table of thread 
-        insert_vme(&thread_current()->vm, vme);
-        ////////// Added in P3 end /////////
-    } else
-      palloc_free_page (kpage);
->>>>>>> 98375323140693ac4cc5dfa9af940c2cead9a45f
     }
 
   return success;
